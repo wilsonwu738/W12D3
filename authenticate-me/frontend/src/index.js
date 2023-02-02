0bf92ee0d5a6e63ca73a9ca5be4ce8903ffb7d8b
Wilson Wu <wuyecheng738@gmail.com>
2023-02-02 13:39:11 -0500

    CSRF set up

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -5,12 +5,14 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
 
 
 const store = configureStore()
 
 if (process.env.NODE_ENV !== 'production') {
-  window.store = store
+  window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
 
@@ -25,9 +27,17 @@ function Root() {
 }
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
+}
